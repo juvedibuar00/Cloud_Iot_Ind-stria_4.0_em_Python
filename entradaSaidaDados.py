1bee5debe02b35b448c634627ba71b35879a14bd
UniFanor <UniFanor@FNS-WS-036731.EDUC.ACAD>
2025-03-27 20:45:10 -0300

    novo envio

---
+++ entradaSaidaDados.py
@@ -0,0 +1,13 @@
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite outro numero: "))
+
+# Operadores aritméticos
+# Adição
+soma = numero1 + numero2
+print(soma)
+# Subtração
+subtracao = numero1-numero2
+print(subtracao)
+# Divisão
+divisao = numero1/numero2
+print(divisao)
---
+++ variaveis.py
@@ -0,0 +1,10 @@
+# nome = 'Luka'
+# Criação e declaraçao de variavel
+# print(nome)
+# Mostrando mensagem na tela.
+
+# Entrada de dados no computador
+
+nomeCompleto = str(input("Qual é o teu nome? "))
+print(nomeCompleto)
+
