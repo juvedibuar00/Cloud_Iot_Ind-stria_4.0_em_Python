ab78e687955e72a031fa411358543cf4dc54b3eb
juvedibuar00 <batchijuve@gmail.com>
2025-04-03 17:34:06 -0300

    aula 03

---
+++ 03-04-2025/indentacao.py
@@ -0,0 +1,15 @@
+# O que é indentacao de codigo
+# Indentação é a prática de adicionar espaços em branco antes de cada linha
+# de código para indicar a estrutura da programação.
+
+# combinaçao de teclas para indentar codigo
+# Tab (tecla Tab) ou Espaço (tecla Espaço)
+
+# Ctrl + Shift + I
+x = 5
+y = 10
+# Indentação de código
+if x > y:
+    print("x é maior que y")
+else:
+    print("y é maior que x")
