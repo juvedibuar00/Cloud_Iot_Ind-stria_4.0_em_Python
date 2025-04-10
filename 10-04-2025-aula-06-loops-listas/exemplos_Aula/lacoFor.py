bbd13a8f0b12fa3f6797ac3a02408f1486db3afe
JUVENALDO FLORENTINO CANJA <61490327363@professores.unifanor.edu.br>
2025-04-10 20:48:05 -0300

    exemplos aula

---
+++ 10-04-2025-aula-06-loops-listas/exemplos_Aula/lacoFor.py
@@ -0,0 +1,11 @@
+# # Nesse programa, iremos demonstrar aplicaçao básica do laço ou loop for
+# for revelino in range (10000):
+#     print(revelino)
+
+# for variavel in range (10):
+#     print("Ola, mundo")
+lista = ["Carlos", "Luka", "Paulo", "Erik", "Adler", "Revelino", "Jose", "Davi", "Vitor"]
+# for nome in lista:
+#     print(nome)
+
+print(lista[0])
